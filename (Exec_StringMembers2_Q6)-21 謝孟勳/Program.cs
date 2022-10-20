f6be511d8464f7127c53982c2541b4542309fde6
ispan <ispan@DESKTOP-PAKSETB>
2022-10-20 12:39:54 +0800

    加入專案檔案。

---
+++ (Exec_StringMembers2_Q6)-21 謝孟勳/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _Exec_StringMembers2_Q6__21_謝孟勳
+{
+    //比對使用者輸入值是否為 "Allen", 區分大小寫
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Allen";
+            if (Regex.IsMatch(name, "[A-Z]") && Regex.IsMatch(name, "[a-z]"))
+            {
+                Console.WriteLine("區分大小寫");
+            }
+            else
+            {
+                Console.WriteLine("沒有區分大小寫");
+            }
+        }
+    }
+}
